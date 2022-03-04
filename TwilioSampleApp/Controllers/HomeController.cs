0c7c66788118e6d4a68951c8815f89cad2230779
hsolgama25697 <hsolgama@intforcesoftware.com>
2022-03-04 21:59:16 +0530

    Intial Commit

---
+++ TwilioSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Twilio.Jwt.AccessToken;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+using TwilioSampleApp.Models;
+
+namespace TwilioSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GenerateToken()
+        {
+             
+            string twilioAccountSid = "AC******************************";
+            string incomingApiKey = "******************************";
+            string incomingApiSecret = "******************************";
+            string outgoingApplicationSid = "AP******************************";
+
+            var grant = new VoiceGrant();
+
+            var grants = new HashSet<IGrant> { { grant } };
+
+            grant.OutgoingApplicationSid = outgoingApplicationSid;
+            grant.IncomingAllow = true;
+            string identity = "JiveLineId";
+            string token = new Token(
+            twilioAccountSid, incomingApiKey,
+            incomingApiSecret, identity,
+            grants: grants).ToJwt();
+             
+            return Json(new {token = token});
+        }
+
+        public IActionResult IncomingCall()
+        {
+            var response = new VoiceResponse();
+            Dial dial = new Dial(callerId: "+1**********");
+            var client = new Client();
+            client.Identity("JiveLineId");
+            dial.Append(client);
+            response.Append(dial);
+            return Content(response.ToString(), "text/xml");
+        }
+
+        public IActionResult OutgoingCall(string phoneNumber)
+        {
+            var response = new VoiceResponse();
+            var dial = new Dial(callerId: "+1**********");
+            dial.Number(phoneNumber: phoneNumber);
+            response.Append(dial);
+            return Content(response.ToString(), "text/xml");
+        }
+         
+        public IActionResult Index()
+        {
+            return View();
+        }
+          
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TwilioSampleApp/wwwroot/js/site.js
@@ -0,0 +1,192 @@
+﻿
+$(document).ready(function () {
+
+    $(".incomingNumber").text("");
+    $('#IMute').hide();
+    $('#IUnMute').hide();
+    $('#IRejectCall').hide();
+    $('#OMute').hide();
+    $('#ICall').hide();
+    $('#OUnMute').hide();
+    $('#ORejectCall').hide();
+      
+    const device = new Twilio.Device();
+
+    $.getJSON("/Home/GenerateToken")
+        .then(function (data) {
+            localStorage.setItem("Token", data.token);
+
+            device.setup(data.token, {
+                codecPreferences: ["opus", "pcmu"],
+                fakeLocalDTMF: true,
+                enableRingingState: true,
+            });
+
+            device.on("ready", function (device) {
+                console.log("Twilio.Device Ready!");
+            });
+
+            device.on("error", function (error) {
+                console.log("Twilio.Device Error: " + error.message);
+            });
+
+            device.on("error", function (error) {
+                console.log("Twilio.Device Error: " + error.message);
+            });
+
+            device.on("connect", function (conn) {
+                $("#IMute").show();
+                $("#IUnMute").hide();
+                $("#IRejectCall").show();
+                reset();
+                conn.reject();
+                console.log('Successfully established call ! ');
+            });
+
+            device.on("disconnect", function (conn) {
+                reset();
+                $("#ICall").show();
+                $(".incomingNumber").show();
+                $("#IMute").hide();
+                $("#IMute").hide();
+                $("#IRejectCall").hide();
+                conn.accept();
+            });
+            reset();
+        })
+        .catch(function (err) {
+            console.log(err);
+            log("Could not get a token from server!");
+        });
+
+
+    // Bind button to make call
+    $('#OCall').bind('click', function () {
+        $("#OCall").hide();
+        $("#OMute").show();
+        $("#ORejectCall").show();
+        var params = {
+            "phoneNumber": $("#outgoingCall").val(),
+        };
+        reset();
+        start();
+        if (device) {
+            var outgoingConnection = device.connect(params);
+            outgoingConnection.on("ringing", function () {
+                console.log("Ringing...");
+            });
+        }
+    })
+     
+    $('#ORejectCall').bind('click', function () {
+        console.log("Call ended.");
+        $("#OMute").hide();
+        $("#ORejectCall").hide();
+        $("#OUnMute").hide();
+        $("#OCall").show();
+        if (device) {
+            device.disconnectAll();
+        }
+    })
+      
+    $('#IMute').click(function () {
+        $('#IMute').hide();
+        $('#IUnMute').show();
+        device.activeConnection().mute(true);
+    });
+
+    $('#IUnMute').click(function () {
+        $('#IMute').hide();
+        $('#IUnMute').show();
+        device.activeConnection().mute(false);
+    });
+     
+    function log(message) {
+        var logDiv = document.getElementById("log");
+        logDiv.innerHTML += "<p>&gt;&nbsp;" + message + "</p>";
+        logDiv.scrollTop = logDiv.scrollHeight;
+    }
+
+    device.on("incoming", function (conn) {
+        reset();
+        debugger;
+        var fromNumber = conn.parameters.From.replace('+1', '');
+        $(".incomingNumber").text(fromNumber);
+        $('#IMute').hide();
+        $('#IUnMute').hide();
+        $("#IRejectCall").show();
+        $("#ICall").show();
+
+        $('#ICall').bind('click', function () {
+            alert("1");
+            $("#IMute").show();
+            $(".incomingNumber").show();
+            $("#IUnMute").hide();
+            $("#IRejectCall").show();
+            $("#ICall").hide();
+            conn.accept();
+            start();
+            
+        })
+
+        $('#IRejectCall').bind('click', function () {
+            reset();
+            alert("1.1");
+            $("#ICall").hide();
+            $(".incomingNumber").hide();
+            $("#ICall").hide();
+            $("#IMute").hide();
+            $("#IUnMute").hide();
+            $("#IRejectCall").hide();
+            reset();
+            conn.reject();
+            device.disconnectAll();
+        })
+    });
+
+    var time = 0;
+    var running = 0;
+
+    function start() {
+        time = 0;
+        running = 0;
+        if (running == 0) {
+            running = 1;
+            increment();
+        } else {
+            running = 0;
+        }
+    }
+
+    function reset() {
+        running = 0;
+        time = 0;
+        $(".divtimer").text("00:00:00");
+    }
+
+    function increment() {
+        if (running == 1) {
+            setTimeout(function () {
+                time++;
+                var mins = Math.floor(time / 10 / 60);
+                var secs = Math.floor(time / 10 % 60);
+                var tenths = time % 10;
+                if (mins < 10) {
+                    mins = "0" + mins;
+                }
+                if (secs < 10) {
+                    secs = "0" + secs;
+                }
+                $(".divtimer").text(mins + ":" + secs + ":" + "0" + tenths);
+                increment();
+            }, 100);
+        }
+    }
+});
+
+var callinterval = "";
+function callingStatus() {
+    callinterval = window.setInterval(function () {
+        CheckCallStatus();
+    }, 5000);
+}
